75adace2c4e2b8c3080fd68dcb795f730d511402
Vladimir <kvv23@tut.by>
2022-01-02 18:52:01 +0300

    создал страницу для отображения пользователей из нескольких компонентов

--- drf_project/frontend/src/App.js
+++ drf_project/frontend/src/App.js
@@ -1,25 +1,46 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UserList from './components/User.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+   constructor(props) {
+       super(props)
+       this.state = {
+           'users': []
+       }
+   }
+
+   componentDidMount() {
+    const users = [
+        {
+            'username': 'admin',
+            'first_name': 'Ivanov',
+            'last_name': 'Ivan',
+            'email': 'ivanov@mail.ru'
+        },
+        {
+          'username': 'admin1',
+          'first_name': 'Petrov',
+          'last_name': 'Petr',
+          'email': 'petrov@mail.ru'
+        },
+    ]
+    this.setState(
+        {
+            'users': users
+        }
+    )
 }
 
+   render () {
+       return (
+           <div>
+            
+            <UserList users={this.state.users} />
+           </div>
+       )
+   }
+}
 export default App;
