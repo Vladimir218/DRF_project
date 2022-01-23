909e954aedc60ee5488588ad2892b3995d17354b
Vladimir <kvv23@tut.by>
2022-01-23 13:59:06 +0300

    lessonon4 организация фильтрации через параметры и библиотеку DjangoFilter

--- drf_project/drf_project/urls.py
+++ drf_project/drf_project/urls.py
@@ -23,6 +23,7 @@ from todoapp.views import ProjectModelViewSet,ToDoModelViewSet
 router = DefaultRouter()
 router.register("customusers", CustomUserModelViewSet, basename='users')
 router.register("projects", ProjectModelViewSet)
+router.register('param', ProjectModelViewSet)
 router.register("todos", ToDoModelViewSet)
 
 
@@ -31,4 +32,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth", include("rest_framework.urls")),
     path("api/", include(router.urls)),
+    path('filters/projects/', include(router.urls)),
 ]
--- drf_project/todoapp/views.py
+++ drf_project/todoapp/views.py
@@ -1,13 +1,32 @@
 from rest_framework.viewsets import ModelViewSet
 from .serializers import ProjectSerializer, ToDoSerializer
 from .models import Project, ToDo
+from rest_framework.renderers import JSONRenderer
 
 
 class ProjectModelViewSet(ModelViewSet):
-    serializer_class = ProjectSerializer
     queryset = Project.objects.all()
+    renderer_classes = [JSONRenderer]
+    serializer_class = ProjectSerializer
+    
+    # организация фильтрации через параметры запроса
+    # фильтрация с частичным совпадением имени проекта
 
+    def get_queryset(self):
+        name = self.request.query_params.get('name', '')
+        projects = Project.objects.all()
+        if name:
+            projects = projects.filter(name__contains=name)
+            
+        return projects
 
 class ToDoModelViewSet(ModelViewSet):
     serializer_class = ToDoSerializer
     queryset = ToDo.objects.all()
+
+    # организация фильтрации через библиотеку DjangoFilter
+    filterset_fields = ['project_id']
+
+    
+
+    
