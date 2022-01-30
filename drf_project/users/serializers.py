8b5297562324414963be3c9d32cb0ddec601ece7
Vladimir <kvv23@tut.by>
2022-01-30 16:16:46 +0300

    lesson5: Добавлено окно при переходе на несуществующую страницу с использованием Switch

--- drf_project/users/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
-from rest_framework.serializers import ModelSerializer
-from .models import CustomUser
-
-class CustomUserModelSerializer(HyperlinkedModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ["username", "first_name", "last_name", "email"]
